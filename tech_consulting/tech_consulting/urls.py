d0f8a9324e2d07d4e381b9a94c0a419b5c72f951
izzy-2023 <izzyzinxhirija@gmail.com>
2024-03-30 15:47:21 -0400

    added urls

--- tech_consulting/tech_consulting/urls.py
+++ tech_consulting/tech_consulting/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+from rest_framework import routers
+from .views import ServiceViewSet, AppointmentViewSet
+
+router = routers.DefaultRouter()
+router.register(r'services', ServiceViewSet)
+router.register(r'appointments', AppointmentViewSet)
+
+urlpatterns = router.urls
+
