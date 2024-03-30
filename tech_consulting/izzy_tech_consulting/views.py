6c1d09cbc40f6f0d3ea0e536d9862a47c45f326f
izzy-2023 <izzyzinxhirija@gmail.com>
2024-03-30 15:59:49 -0400

    updated views

--- tech_consulting/izzy_tech_consulting/views.py
+++ tech_consulting/izzy_tech_consulting/views.py
@@ -1,13 +1,15 @@
 from rest_framework import viewsets
 from .models import Service, Appointment
 from .serializers import ServiceSerializer, AppointmentSerializer
-
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated
 
 class ServiceViewSet(viewsets.ModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
+    permission_classes = [IsAuthenticated]
 
 class AppointmentViewSet(viewsets.ModelViewSet):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
+    permission_classes = [IsAuthenticated]
+
